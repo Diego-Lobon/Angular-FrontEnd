232e09735709c1b07396df07d9e1780b1160ba40
Diego-Lobon <diegolima488@gmail.com>
2026-06-18 09:57:46 -0500

    netifly v1

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,14 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    // Esto le dice a Angular: "No intentes pre-renderizar en el build nada que empiece con home-list-price"
+    {
+        path: 'home-list-price/**',
+        renderMode: RenderMode.Server,
+    },
+    // El resto de las rutas pueden seguir usando el comportamiento por defecto (Prerender)
+    {
+        path: '**',
+        renderMode: RenderMode.Prerender,
+    },
 ];
