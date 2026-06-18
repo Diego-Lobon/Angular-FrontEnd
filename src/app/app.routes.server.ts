3ee199823efd99e6afda3ac22563f278e228ef52
Diego-Lobon <diegolima488@gmail.com>
2026-06-18 10:05:39 -0500

    Netlify v2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,12 +1,6 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-    // Esto le dice a Angular: "No intentes pre-renderizar en el build nada que empiece con home-list-price"
-    {
-        path: 'home-list-price/**',
-        renderMode: RenderMode.Server,
-    },
-    // El resto de las rutas pueden seguir usando el comportamiento por defecto (Prerender)
     {
         path: '**',
         renderMode: RenderMode.Prerender,
